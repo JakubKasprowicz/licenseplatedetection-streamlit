64b0a973567ac12fc6792f0177bc3fd9cbae1577
JakubKasprowicz <80197721+JakubKasprowicz@users.noreply.github.com>
2023-05-04 13:17:56 +0200

    Add files via upload

---
+++ streamlit_main.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+# Set page title
+st.set_page_config(page_title="My Streamlit App")
+
+# Add page header
+st.title("Welcome to my Streamlit app")
+
+# Add some text
+st.write("This is a simple example of a Streamlit app.")
+
+# Add an image
+st.image("https://upload.wikimedia.org/wikipedia/commons/thumb/6/63/Streamlit_logo_share.png/1200px-Streamlit_logo_share.png", caption="Streamlit logo", width=200)
+
+# Add some more text
